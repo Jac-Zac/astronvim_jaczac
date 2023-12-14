0c29dcda57cb109c335ffad710dda2d7c3e07dc5
Jacopo Zacchigna <jacopozac@icloud.com>
2023-12-14 22:18:58 +0100

    changed order

--- plugins/alpha.lua
+++ plugins/alpha.lua
@@ -29,8 +29,8 @@ return {
 
     local button, get_icon = require("astronvim.utils").alpha_button, require("astronvim.utils").get_icon
     dashboard.section.buttons.val = {
-      button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
       button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
       button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
       button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
       button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
