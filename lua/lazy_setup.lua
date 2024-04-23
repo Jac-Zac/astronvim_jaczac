290d7f5d1e4e1ba87bbfa65058246a9d6d5260d6
Jac-Zac <jacopozac@icloud.com>
2024-04-23 15:59:24 +0200

    Update to nvim 4. something

---
+++ lua/lazy_setup.lua
@@ -0,0 +1,36 @@
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
+    import = "astronvim.plugins",
+    opts = { -- AstroNvim options must be set here with the `import` key
+      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
+      tabstop = 4, -- number of space in a tab
+      showtabline = 4, -- always display tabline
+      -- foldlevel = 99, -- allow multiple folds
+      -- foldlevelstart = 99, -- start with all code unfolded
+    },
+  },
+  { import = "community" },
+  { import = "plugins" },
+} --[[@as LazySpec]], {
+  -- Configure any other `lazy.nvim` configuration options here
+  install = { colorscheme = { "astrodark", "habamax", "nord" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins, add more to your liking
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+} --[[@as LazyConfig]])
