3ffc18d89d39f873b7a6af6bd02f0d805e1de78c
Jac-Zac <jacopozac@icloud.com>
2024-05-25 22:58:51 +0200

    Preparing for noice

--- lua/community.lua
+++ lua/community.lua
@@ -16,6 +16,7 @@ return {
   {
     "EdenEast/nightfox.nvim",
     opts = {
+      integrations = { noice = true },
       options = {
         module_default = false,
         modules = {
@@ -42,7 +43,7 @@ return {
   {
     "catppuccin/nvim",
     optional = true,
-    opts = { integrations = { leap = true } },
+    opts = { integrations = { noice = true } },
   },
 
   -- Import flash for better movement
@@ -102,8 +103,22 @@ return {
     },
     opts = {},
   },
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- add any options here
+  --   },
+  --   dependencies = {
+  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+  --     "MunifTanjim/nui.nvim",
+  --     -- OPTIONAL:
+  --     --   `nvim-notify` is only needed, if you want to use the notification view.
+  --     --   If not available, we use `mini` as the fallback
+  --     "rcarriga/nvim-notify",
+  --   },
+  -- },
 
-  -- Setting up Noice to make things nicer
   -- {
   --   "folke/noice.nvim",
   --   event = "VeryLazy",
@@ -113,20 +128,18 @@ return {
   --     return utils.extend_tbl(opts, {
   --       lsp = {
   --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --         override = {
-  --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --           ["vim.lsp.util.stylize_markdown"] = true,
-  --           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-  --           ["config.lsp.signature.enabled"] = false,
-  --         },
+  --         -- override = {
+  --         --   ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+  --         --   ["vim.lsp.util.stylize_markdown"] = true,
+  --         --   ["cmp.entry.get_documentation"] = true,
+  --         -- },
   --       },
-  --       -- you can enable a preset for easier configuration
   --       presets = {
-  --         bottom_search = true, -- use a classic bottom cmdline for search
-  --         command_palette = true, -- position the cmdline and popupmenu together
-  --         long_message_to_split = true, -- long messages will be sent to a split
-  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-  --         lsp_doc_border = false, -- add a border to hover docs and signature help
+  --         -- bottom_search = true, -- use a classic bottom cmdline for search
+  --         -- command_palette = true, -- position the cmdline and popupmenu together
+  --         -- long_message_to_split = true, -- long messages will be sent to a split
+  --         -- inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
+  --         -- lsp_doc_border = false, -- add a border to hover docs and signature help
   --       },
   --     })
   --   end,
