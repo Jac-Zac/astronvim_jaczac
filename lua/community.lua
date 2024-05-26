d9e1aaab8a8556152e953d7ffcff0131bb6e035b
Jac-Zac <jacopozac@icloud.com>
2024-05-26 10:00:59 +0200

    Updating small thing

--- lua/community.lua
+++ lua/community.lua
@@ -159,9 +159,8 @@ return {
         },
         presets = {
           bottom_search = true, -- use a classic bottom cmdline for search
-          -- command_palette = false, -- position the cmdline and popupmenu together
           -- Set to false to make it appear in the center
-          command_palette = true, -- position the cmdline and popupmenu together
+          -- command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
           lsp_doc_border = false, -- add a border to hover docs and signature help
