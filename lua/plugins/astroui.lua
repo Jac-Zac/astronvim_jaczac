635a1e928c9000d1d7376b66a43bfe0352de0cbe
Jac-Zac <jacopozac@icloud.com>
2024-05-30 10:03:35 +0200

    Using nord again

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -12,7 +12,7 @@ return {
   opts = {
     -- change colorscheme
     Spellcheck = "en_uk",
-    colorscheme = "catppuccin-frappe",
+    colorscheme = "nordfox",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
