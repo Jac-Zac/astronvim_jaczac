1bca57429bd844ba3502fdb78b768691520548a8
Jac-Zac <jacopozac@icloud.com>
2024-05-30 10:01:56 +0200

    Fixing catpuccino

--- lua/community.lua
+++ lua/community.lua
@@ -42,8 +42,33 @@ return {
   -- Catpuccino theme
   {
     "catppuccin/nvim",
-    optional = true,
-    opts = { integrations = { noice = true } },
+    name = "catppuccin",
+    ---@type CatppuccinOptions
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dap = true,
+        dap_ui = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = true,
+        markdown = true,
+        mason = true,
+        native_lsp = { enabled = true },
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        treesitter = true,
+        ts_rainbow = false,
+        ufo = true,
+        which_key = true,
+        window_picker = true,
+      },
+    },
   },
 
   -- Import flash for better movement
@@ -104,32 +129,6 @@ return {
     opts = {},
   },
 
-  -- Noice nvim
-  -- {
-  --   "folke/noice.nvim",
-  --   event = "VeryLazy",
-  --   dependencies = { "MunifTanjim/nui.nvim" },
-  --   opts = function(_, opts)
-  --     local utils = require "astrocore"
-  --     return utils.extend_tbl(opts, {
-  --       lsp = {
-  --         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-  --         override = {
-  --           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --           ["vim.lsp.util.stylize_markdown"] = true,
-  --           ["cmp.entry.get_documentation"] = true,
-  --         },
-  --       },
-  --       presets = {
-  --         bottom_search = true, -- use a classic bottom cmdline for search
-  --         command_palette = true, -- position the cmdline and popupmenu together
-  --         long_message_to_split = true, -- long messages will be sent to a split
-  --         inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
-  --         lsp_doc_border = false, -- add a border to hover docs and signature help
-  --       },
-  --     })
-  --   end,
-  -- },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,8 +11,8 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    Spellcheck = "en_us",
-    colorscheme = "nordfox",
+    Spellcheck = "en_uk",
+    colorscheme = "catppuccin-frappe",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
