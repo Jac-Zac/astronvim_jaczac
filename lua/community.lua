92da20a21f4441fb544cc9e4646ffa427a0b86bc
Jac-Zac <jacopozac@icloud.com>
2024-05-31 22:16:38 +0200

    Setting up tina config

--- lua/community.lua
+++ lua/community.lua
@@ -181,9 +181,9 @@ return {
           },
         },
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
+          bottom_search = false, -- use a classic bottom cmdline for search
           -- Set to false to make it appear in the center
-          -- command_palette = true, -- position the cmdline and popupmenu together
+          command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
           lsp_doc_border = false, -- add a border to hover docs and signature help
@@ -191,9 +191,9 @@ return {
 
         -- Set the command line to the buttom
         -- Comment it to make it appear in the center
-        cmdline = {
-          view = "cmdline",
-        },
+        -- cmdline = {
+        --   view = "cmdline",
+        -- },
       })
     end,
   },
