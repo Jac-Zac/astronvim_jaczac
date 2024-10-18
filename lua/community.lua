454c2736dcc4d7b21f625567cf2942b4342041fd
Jac-Zac <jacopozac@icloud.com>
2024-10-18 09:13:02 +0200

    Adding back markdown preview

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,16 @@ return {
   --
   { import = "astrocommunity.pack.python" },
 
+  -- Markdown preview
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  },
+
   -- Virtual text for nvim dap
   {
     "theHamsta/nvim-dap-virtual-text",
