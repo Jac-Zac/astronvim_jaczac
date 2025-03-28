e7f685cc87e46f93eda909b75b4dbab6ea70d30f
Jac-Zac <jacopozac@icloud.com>
2025-03-28 13:27:07 +0100

    Configuration for tina

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -15,7 +15,7 @@ return {
         },
       },
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
+        bottom_search = false, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
@@ -24,9 +24,9 @@ return {
 
       -- Set the command line to the buttom
       -- Comment it to make it appear in the center
-      cmdline = {
-        view = "cmdline",
-      },
+      -- cmdline = {
+      --   view = "cmdline",
+      -- },
     })
   end,
   specs = {
