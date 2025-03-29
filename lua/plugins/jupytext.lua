1b559ca53bf30b4f50fa944e7034aaa165d0ed2e
Jac-Zac <jacopozac@icloud.com>
2025-03-29 08:53:06 +0100

    Adding plugins

---
+++ lua/plugins/jupytext.lua
@@ -0,0 +1,26 @@
+---@type LazySpec
+return {
+  "GCBallesteros/jupytext.nvim",
+  config = true,
+  opts = {
+    -- custom_language_formatting = {
+    style = "markdown",
+    output_extension = "md",
+    force_ft = "markdown",
+    -- style = "quarto",
+    -- output_extension = "qmd",
+    -- force_ft = "quarto",
+
+    -- python = {
+    --   extension = "qmd",
+    --   style = "quarto",
+    --   force_ft = "quarto",
+    -- },
+    -- r = {
+    --   extension = "qmd",
+    --   style = "quarto",
+    --   force_ft = "quarto",
+    -- },
+    -- },
+  },
+}
