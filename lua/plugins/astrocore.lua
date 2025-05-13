b0d45d1ed392e5e67b853fa29e107c74695190b9
Jac-Zac <jacopozac@icloud.com>
2025-05-13 14:34:55 +0200

    Snell setup

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -13,7 +13,7 @@ return {
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
+      autopairs = false, -- enable autopairs at start
       cmp = true, -- enable completion at start
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
       highlighturl = true, -- highlight URLs at start
