b8a2aafb8d180953c316638fc939871ccee5110e
Jac-Zac <jacopozac@icloud.com>
2025-09-16 12:27:18 +0200

    Adding copilot

--- lua/community.lua
+++ lua/community.lua
@@ -27,6 +27,9 @@ return {
   { import = "astrocommunity.file-explorer.mini-files" },
   { import = "astrocommunity.editing-support.undotree" },
 
+  -- AI completion
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+
   -- Discover nice motions thanks to precognition-nvim
   -- { import = "astrocommunity.workflow.precognition-nvim" },
 
