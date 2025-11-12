421079b69a0b9dd507eef047b2f1b4f2e053e26e
Jac-Zac <jacopozac@icloud.com>
2025-11-12 19:38:50 +0100

    Updated code

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -1,7 +1,7 @@
 return {
   {
     "Vigemus/iron.nvim",
-    ft = { "python" },
+    ft = { "python", "markdown" },
 
     config = function()
       local iron = require "iron.core"
@@ -22,7 +22,16 @@ return {
             },
             python = {
               format = require("iron.fts.common").bracketed_paste,
-              command = { "ipython", "-i", "--no-autoindent", "--nosep", " --quiet" },
+              command = {
+                "ipython",
+                "-i",
+                "--no-autoindent",
+                "--nosep",
+                "--quiet",
+                "--no-confirm-exit",
+                "--no-tip",
+                "-no-banner",
+              },
               env = { PYTHON_BASIC_REPL = "1" }, --this is needed for python3.13 and up.
               block_dividers = { "# %%", "#%%" },
             },
@@ -54,14 +63,21 @@ return {
         function() iron.repl_for(vim.bo.filetype) end,
         { desc = "REPL: start/toggle/hide (from code buffer)" }
       )
-
-      -- vim.keymap.set("n", "<leader>rc", iron.clear, { desc = "REPL: clear (from code buffer)" })
-      -- vim.keymap.set("n", "<C-CR>", iron.send_code_block, { desc = "Send Code Block" })
+      vim.keymap.set(
+        "n",
+        "<leader>rC",
+        function() iron.close_repl(vim.bo.filetype) end,
+        { desc = "REPL: close (from code buffer)" }
+      )
 
       vim.keymap.set("n", "<leader>rl", iron.send_line, { desc = "Run line" })
       vim.keymap.set("n", "<leader>rp", iron.send_paragraph, { desc = "Run paragraph" })
       vim.keymap.set("n", "<leader>ru", iron.send_until_cursor, { desc = "Run until cursor" })
       vim.keymap.set("n", "<leader>rf", iron.send_file, { desc = "Run file" })
+      vim.keymap.set("n", "<C-CR>", iron.send_code_block, { desc = "Send Code Block" })
+
+      ---- from inside REPL only
+      vim.keymap.set("n", "<leader>rR", iron.repl_restart, { desc = "REPL: restart (from inside REPL)" })
 
       -- visual mode
       vim.keymap.set("v", "<C-CR>", iron.visual_send, { desc = "Run selection" })
